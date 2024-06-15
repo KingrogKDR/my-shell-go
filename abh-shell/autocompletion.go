698d3929a6433974eb3156db3f6f2d63510546d0
KingrogKDR <abhisheksaikia766@gmail.com>
2024-06-15 13:32:41 +0530

    add autocompletion

---
+++ abh-shell/autocompletion.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+    "os"
+    "fmt"
+    "github.com/spf13/cobra"
+)
+
+func autocompletion() {
+    rootcmd := &cobra.Command{
+        Use: "abh-shell",
+        Short: "Kingrog's shell",
+    }
+
+    // bash-completion
+    rootcmd.AddCommand(&cobra.Command{
+        Use: "bash-completion",
+        Short: "Generate bash completion script",
+        Run: func (cmd *cobra.Command, args []string)  {
+            rootcmd.GenBashCompletion(os.Stdout)
+        },
+    })
+
+    // zsh-completion
+    rootcmd.AddCommand(&cobra.Command{
+        Use: "zsh-completion",
+        Short: "Generate zsh completion script",
+        Run: func (cmd *cobra.Command, args []string)  {
+            rootcmd.GenZshCompletion(os.Stdout)
+        },
+    })
+
+    // fish-completion
+    rootcmd.AddCommand(&cobra.Command{
+        Use: "fish-completion",
+        Short: "Generate fish completion script",
+        Run: func (cmd *cobra.Command, args []string)  {
+            rootcmd.GenFishCompletion(os.Stdout, true)
+        },
+    })
+
+    // powershell-completion
+    rootcmd.AddCommand(&cobra.Command{
+        Use: "powershell-completion",
+        Short: "Generate powershell completion script",
+        Run: func (cmd *cobra.Command, args []string)  {
+            rootcmd.GenPowerShellCompletionWithDesc(os.Stdout)
+        },
+    })
+
+    if err := rootcmd.Execute(); err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+
+}
---
+++ abh-shell/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+func exists(slice []string, element string) bool{
+    for _, x := range slice{
+        if x == element{
+            return true
+        }
+    }
+    return false
+}
+
+func executablePath(commands []string) {
+    env, ok := os.LookupEnv("PATH")
+    if ok {
+        paths := strings.Split(env, ":")
+        for _, path := range paths {
+            execPath := path + "/" + commands[1]
+            if _, err := os.Stat(execPath); err == nil {
+                fmt.Fprintf(os.Stdout, "%s is %s\n", commands[1], execPath)
+                return
+            }
+        }
+    } 
+    fmt.Fprintf(os.Stdout, "%s: not found\n", commands[1])
+    
+}
+
+func main() {
+    // Uncomment this block to pass the first stage
+
+    builtins := []string{"echo", "exit", "type"}
+
+    for{
+
+        fmt.Fprint(os.Stdout, "$ ")
+
+        // Wait for user input
+        command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+        if err != nil {
+            log.Fatal(err);
+        }
+
+        command = strings.TrimSpace(command)
+
+        cmd := strings.Split(command, " ")
+
+        
+        switch cmd[0] {
+        case "exit":
+            if len(cmd) == 1{
+                os.Exit(0)
+            }
+            code, err := strconv.Atoi(cmd[1])
+            if err != nil {
+                log.Fatal(err)
+            }
+            os.Exit(code)
+        case "echo":
+            fmt.Fprintln(os.Stdout, strings.Join(cmd[1:], " "))
+        case "type" :
+            if exists(builtins, cmd[1]) {
+                fmt.Printf("%s is a shell builtin\n", cmd[1])    
+            } else {
+                executablePath(cmd)
+            }
+        case "cd":
+            if cmd[1] == "~" {
+                env, ok := os.LookupEnv("HOME")
+                if ok {
+                    os.Chdir(env)
+                }
+            } else if err:=os.Chdir(cmd[1]); err!=nil {
+                fmt.Fprintf(os.Stdout, "%s: No such file or directory\n", cmd[1])
+            }
+        default:
+            cmand := exec.Command(cmd[0], cmd[1:]...)
+            cmand.Stdout = os.Stdout
+            cmand.Stderr = os.Stderr
+            cmand.Stdin = os.Stdin
+            if err:=cmand.Run(); err!=nil {
+                fmt.Fprintf(os.Stdout, "%s: command not found\n", cmd[0])
+            }
+        }
+    }
+}
